04eb8e9fe1b69226d32fd57e4897865f69f649c6
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-06 10:06:45 +0100

    cards toggling functionnality added

--- scripts-cards.js
+++ scripts-cards.js
@@ -21,15 +21,52 @@ export function displayTarot(){
                 let card = document.createElement('div');
                 card.classList.add('tarot');
                 card.setAttribute('onclick', 'this.classList.toggle("activecard");');
-        
+                
+                let cardinner = document.createElement('div');
+                cardinner.classList.add('card-inner');
+                card.appendChild(cardinner);
+                
+                let cardfront = document.createElement('div');
+                cardfront.classList.add('card-front');
+                cardinner.appendChild(cardfront);
+
                 let image = document.createElement('img');
                 image.classList.add('cartes');
                 image.src = './assets/cartes/'+element.filename;
                 image.alt = element.name;
-                card.appendChild(image);
+                cardfront.appendChild(image);
+
+                let cardback = document.createElement('div');
+                cardback.classList.add('card-back');
+                cardinner.appendChild(cardback);
+
+                let effect = document.createElement('div');
+                effect.classList.add('effect');
+                effect.innerHTML = element.effect;
+                cardback.appendChild(effect);
+
+                let proba = document.createElement('div');
+                proba.classList.add('proba');
+                proba.innerHTML = "Probabilité : "+element.probability;
+                cardback.appendChild(proba);
 
                 contentWrapper.appendChild(card);
             });
             grid.appendChild(card);
         })
+}
+
+function probabilityProgressBar(id, percent, legend=""){
+    $(document).ready(function () {
+        var progressBar = $('#'+id).lineProgressBar({
+            numOfLines: 10,
+            showPercent: false,
+            lineColor: "#82f7ff",
+            completeColor: "#0094cc",
+            showLegend: legend==="" ? false : true,
+            legend: legend,
+        });
+        
+        progressBar.update(percent);
+    });
 }
